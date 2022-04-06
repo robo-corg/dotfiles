65ebd4d3f841e5c30de233a11b8f4fdde3951698
Andrew McHarg <amcharg@octoml.ai>
2022-04-06 12:44:36 -0700

    Redoing dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/amcharg/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/amcharg/Downloads/google-cloud-sdk/path.fish.inc'; end
