fa53001903726c25d6ade1e4e0ada20aec3a4bea
Andrew McHarg <amcharg@gmail.com>
2022-04-16 19:05:42 -0700

    Cleanup fish paths

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,7 @@ end
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/amcharg/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/amcharg/Downloads/google-cloud-sdk/path.fish.inc'; end
+
+fish_add_path ~/.poetry/bin
+fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin
